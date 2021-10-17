77aaa392a3f05038bb1fa252624094794673f049
J.W. Jagersma <jwjagersma@gmail.com>
2021-10-18 00:29:22 +0200

    implement function ctors for nullptr and different-sized function types

--- include/jw/function.h
+++ include/jw/function.h
@@ -22,6 +22,11 @@ namespace jw::detail
     };
     template<typename F>
     functor(F) -> functor<std::remove_cvref_t<F>>;
+
+    template<typename T>
+    struct detect_function_instance : std::false_type { };
+    template<typename F>
+    concept is_function_instance = detect_function_instance<std::remove_cvref_t<F>>::value;
 }
 
 namespace jw
@@ -30,7 +35,10 @@ namespace jw
     struct function;
 
     // A simple std::function alternative that never allocates.  It contains
-    // enough space to store a lambda that captures N pointers or references.
+    // enough space to store a lambda that captures N pointer-sized objects.
+    // Note that the copy/move assignment operators for the lambda's captured
+    // objects will never be called!  Therefore only trivial types, such as
+    // pointers and references, can safely be captured.
     template<typename R, typename... A, unsigned N>
     struct function<R(A...), N>
     {
@@ -41,8 +49,13 @@ namespace jw
         function& operator=(function&&) noexcept = default;
         function& operator=(const function&) noexcept = default;
 
-        template<typename F>
+        function(std::nullptr_t) noexcept : function { } { }
+
+        template<typename F> requires (not detail::is_function_instance<F>)
         function(F&& func) : function { create(std::forward<F>(func)) } { }
+
+        template<unsigned M> requires (M <= N)
+        function(const function<R(A...), M>& other) : storage { other.storage }, call { other.call } { }
 
         R operator()(A... args) const { return call(&storage, std::forward<A>(args)...); }
 
@@ -54,8 +67,7 @@ namespace jw
         static function create(F&& func)
         {
             auto wrapper = detail::functor { std::forward<F>(func) };
-            static_assert(std::is_trivially_copyable_v<std::remove_cvref_t<F>>);
-            static_assert(std::is_trivially_destructible_v<std::remove_cvref_t<F>>);
+            static_assert(std::is_trivially_destructible_v<decltype(wrapper)>);
             static_assert(sizeof(wrapper) <= sizeof(storage));
             static_assert(alignof(wrapper) <= alignof(storage));
             function f;
@@ -73,4 +85,10 @@ namespace jw
 
     template<typename F, typename Signature = typename std::__function_guide_helper<decltype(&F::operator())>::type>
     function(F) -> function<Signature, (sizeof(F) - 1) / sizeof(void*) + 1>;
+}
+
+namespace jw::detail
+{
+    template<typename T, unsigned N>
+    struct detect_function_instance<function<T, N>> : std::true_type { };
 }
