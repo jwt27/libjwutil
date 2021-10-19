1ed0dc83794255b45919b545584f58d87401a5f1
J.W. Jagersma <jwjagersma@gmail.com>
2021-10-19 08:19:58 +0200

    implement monomorphic_allocator

--- include/jw/alloc.h
+++ include/jw/alloc.h
@@ -52,6 +52,116 @@ namespace jw
         return std::unique_ptr<T, deleter> { nullptr, deleter { rebind { alloc } } };
     }
 
+    // Behaves much like std::pmr::polymorphic_allocator, except that the
+    // specific type of memory_resource can be narrowed down to eliminate
+    // virtual function calls.
+    template<typename Resource = std::pmr::memory_resource, typename T = std::byte>
+    requires (std::is_base_of_v<std::pmr::memory_resource, Resource>)
+    struct monomorphic_allocator
+    {
+        using value_type = T;
+        using resource_type = Resource;
+
+        using propagate_on_container_copy_assignment = std::false_type;
+        using propagate_on_container_move_assignment = std::false_type;
+        using propagate_on_container_swap = std::false_type;
+        template <typename U> struct rebind { using other = monomorphic_allocator<Resource, U>; };
+
+        constexpr monomorphic_allocator() = default;
+        constexpr monomorphic_allocator(const monomorphic_allocator&) noexcept = default;
+        constexpr monomorphic_allocator(Resource* res) noexcept : r { res } { };
+
+        template<typename R2, typename U>
+        requires (std::is_base_of_v<Resource, R2>)
+        constexpr monomorphic_allocator(const monomorphic_allocator<R2, U>& other) noexcept : r { other.resource() } { }
+
+        template<typename U>
+        requires (std::is_same_v<std::pmr::memory_resource, Resource>)
+        constexpr monomorphic_allocator(const std::pmr::polymorphic_allocator<U>& other) noexcept : r { other.resource() } { }
+
+        template<typename U>
+        operator std::pmr::polymorphic_allocator<U>() { return std::pmr::polymorphic_allocator<U> { r }; }
+
+        monomorphic_allocator& operator=(const monomorphic_allocator&) = delete;
+
+        [[nodiscard]] T* allocate(std::size_t n)
+        {
+            return static_cast<T*>(allocate_bytes(n * sizeof(T), alignof(T)));
+        }
+
+        void deallocate(T* p, std::size_t n)
+        {
+            deallocate_bytes(p, n * sizeof(T), alignof(T));
+        }
+
+        [[nodiscard]] void* allocate_bytes(std::size_t n, std::size_t a = alignof(std::max_align_t))
+        {
+            return r->allocate(n, a);
+        }
+
+        void deallocate_bytes(void* p, std::size_t n, std::size_t a = alignof(std::max_align_t))
+        {
+            r->deallocate(p, n, a);
+        }
+
+        template <typename U>
+        [[nodiscard]] U* allocate_object(std::size_t n = 1)
+        {
+            return static_cast<U*>(allocate_bytes(n * sizeof(U), alignof(U)));
+        }
+
+        template <typename U>
+        void deallocate_object(U* p, std::size_t n = 1)
+        {
+            deallocate_bytes(p, n * sizeof(U), alignof(U));
+        }
+
+        template <typename U, typename... A>
+        void construct(U* p, A&&... args)
+        {
+            std::uninitialized_construct_using_allocator(p, *this, std::forward<A>(args)...);
+        }
+
+        template<typename U>
+        void destroy(U* p)
+        {
+            p->~U();
+        }
+
+        template <typename U, typename... A>
+        [[nodiscard]] U* new_object(A&&... args)
+        {
+            auto* const p = allocate_object<U>();
+            try { construct<U>(p, std::forward<A>(args)...); }
+            catch (...) { deallocate_object(p); throw; }
+            return p;
+        }
+
+        template <typename U>
+        void delete_object(U* p)
+        {
+            destroy(p);
+            deallocate_object(p);
+        }
+
+        template <typename R2, typename U>
+        bool operator==(const monomorphic_allocator<R2, U>& b) const noexcept
+        {
+            return resource() == b.resource() or resource()->is_equal(*b.resource());
+        }
+
+        template <typename U>
+        bool operator==(const std::pmr::polymorphic_allocator<U>& b) const noexcept
+        {
+            return resource() == b.resource() or resource()->is_equal(*b.resource());
+        }
+
+        constexpr Resource* resource() const noexcept { return r; }
+
+    private:
+        Resource* r;
+    };
+
     // A std::pmr::memory_resource which allocates from one or multiple pools.  It is implemented
     // as a binary tree which is horizontally ordered by address, and vertically sorted by size.
     // The pool size can be increased dynamically by feeding pointers to grow().  Note that this
