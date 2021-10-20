aed31de777e7c9ca2af02e65aab2bf22720acc87
J.W. Jagersma <jwjagersma@gmail.com>
2021-10-20 08:42:45 +0200

    rename function -> trivial_function

--- include/jw/function.h
+++ include/jw/function.h
@@ -42,7 +42,7 @@ namespace jw::detail
 namespace jw
 {
     template<typename, unsigned = 1>
-    struct function;
+    struct trivial_function;
 
     // A simple std::function alternative that never allocates.  It contains
     // enough space to store a lambda that captures N pointer-sized objects.
@@ -50,25 +50,25 @@ namespace jw
     // objects will never be called!  Therefore only trivial types, such as
     // pointers and references, can safely be captured.
     template<typename R, typename... A, unsigned N>
-    struct function<R(A...), N>
+    struct trivial_function<R(A...), N>
     {
-        function() noexcept = default;
-        ~function() = default;
-        function(function&&) noexcept = default;
-        function(const function&) noexcept = default;
-        function& operator=(function&&) noexcept = default;
-        function& operator=(const function&) noexcept = default;
+        trivial_function() noexcept = default;
+        ~trivial_function() = default;
+        trivial_function(trivial_function&&) noexcept = default;
+        trivial_function(const trivial_function&) noexcept = default;
+        trivial_function& operator=(trivial_function&&) noexcept = default;
+        trivial_function& operator=(const trivial_function&) noexcept = default;
 
-        function(std::nullptr_t) noexcept : function { } { }
+        trivial_function(std::nullptr_t) noexcept : trivial_function { } { }
 
         template<typename F> requires (not detail::is_function_instance<std::remove_cvref_t<F>>)
-        explicit function(F&& func) : function { create(std::forward<F>(func)) } { }
+        explicit trivial_function(F&& func) : trivial_function { create(std::forward<F>(func)) } { }
 
         template<typename F>
-        function& operator=(F&& func) noexcept { return *this = function { std::forward<F>(func) }; };
+        trivial_function& operator=(F&& func) noexcept { return *this = trivial_function { std::forward<F>(func) }; };
 
         template<unsigned M> requires (M < N)
-        function(const function<R(A...), M>& other) noexcept : function { copy(other) } { }
+        trivial_function(const trivial_function<R(A...), M>& other) noexcept : trivial_function { copy(other) } { }
 
         R operator()(A... args) const { return call(&storage, std::forward<A>(args)...); }
 
@@ -77,41 +77,41 @@ namespace jw
 
     private:
         template<typename F>
-        static function create(F&& func)
+        static trivial_function create(F&& func)
         {
             auto wrapper = detail::functor { std::forward<F>(func) };
             static_assert(std::is_trivially_destructible_v<decltype(wrapper)>);
             static_assert(sizeof(wrapper) <= sizeof(storage));
             static_assert(alignof(wrapper) <= alignof(storage));
-            function f;
+            trivial_function f;
             new (&f.storage) decltype(wrapper) { std::move(wrapper) };
             f.call = &decltype(wrapper)::template call<R, A...>;
             return f;
         }
 
         template<unsigned M>
-        static function copy(const function<R(A...), M>& other) noexcept
+        static trivial_function copy(const trivial_function<R(A...), M>& other) noexcept
         {
-            function f;
+            trivial_function f;
             std::memcpy(&f.storage, &other.storage, sizeof(other.storage));
             f.call = other.call;
             return f;
         }
 
-        template<typename, unsigned> friend struct function;
-        using dummy_functor = detail::functor<decltype([x = std::declval<std::array<void*, N>>()](A...) { })>;
-        using storage_t = std::aligned_storage_t<sizeof(dummy_functor), alignof(dummy_functor)>;
+        template<typename, unsigned> friend struct trivial_function;
+        using dummy = detail::functor<decltype([x = std::declval<std::array<void*, N>>()](A...) { })>;
+        using storage_t = std::aligned_storage_t<sizeof(dummy), alignof(dummy)>;
 
         storage_t storage;
         R(*call)(const void*, A...) { nullptr };
     };
 
     template<typename F, typename Signature = typename detail::member_function_signature<decltype(&F::operator())>::type>
-    function(F) -> function<Signature, (sizeof(F) - 1) / sizeof(void*) + 1>;
+    trivial_function(F) -> trivial_function<Signature, (sizeof(F) - 1) / sizeof(void*) + 1>;
 }
 
 namespace jw::detail
 {
     template <typename Sig, unsigned N>
-    inline constexpr bool is_function_instance<function<Sig, N>> = true;
+    inline constexpr bool is_function_instance<trivial_function<Sig, N>> = true;
 }
