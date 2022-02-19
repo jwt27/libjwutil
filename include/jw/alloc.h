ab025625087b96a17685ef5036e5dc21845fb023
J.W. Jagersma <jwjagersma@gmail.com>
2022-02-19 18:30:07 +0100

    add uses_allocator_adaptor

--- include/jw/alloc.h
+++ include/jw/alloc.h
@@ -82,6 +82,34 @@ namespace jw
     template<typename T>
     using default_constructing_allocator = default_constructing_allocator_adaptor<std::allocator<T>>;
 
+    // An allocator adaptor that applies uses-allocator construction on its
+    // elements.
+    // For example, in a std::vector<std::basic_string<...>, ...>, the string
+    // elements will be constructed with the same allocator as used for the
+    // vector, if the allocators are compatible.
+    template<typename A>
+    struct uses_allocator_adaptor : A
+    {
+        using value_type = std::allocator_traits<A>::value_type;
+        using size_type = std::allocator_traits<A>::size_type;
+        using difference_type = std::allocator_traits<A>::difference_type;
+        using pointer = std::allocator_traits<A>::pointer;
+        using const_pointer = std::allocator_traits<A>::const_pointer;
+        using void_pointer = std::allocator_traits<A>::void_pointer;
+        using const_void_pointer = std::allocator_traits<A>::const_void_pointer;
+
+        using propagate_on_container_copy_assignment = std::allocator_traits<A>::propagate_on_container_copy_assignment;
+        using propagate_on_container_move_assignment = std::allocator_traits<A>::propagate_on_container_move_assignment;
+        using propagate_on_container_swap = std::allocator_traits<A>::propagate_on_container_swap;
+        template <typename T> struct rebind { using other = uses_allocator_adaptor<typename std::allocator_traits<A>::rebind_alloc<T>>; };
+
+        template <typename T, typename... Args>
+        void construct(T* p, Args&&... args)
+        {
+            std::uninitialized_construct_using_allocator(p, *this, std::forward<A>(args)...);
+        }
+    };
+
     // Behaves much like std::pmr::polymorphic_allocator, except that the
     // specific type of memory_resource can be narrowed down to eliminate
     // virtual function calls.
