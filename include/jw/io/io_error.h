eaa13eccf728c389a57a093b629b3c1bfc4d7cd6
J.W. Jagersma <jwjagersma@gmail.com>
2023-05-29 15:57:20 +0200

    remove line_break

--- include/jw/io/io_error.h
+++ include/jw/io/io_error.h
@@ -1,4 +1,5 @@
 /* * * * * * * * * * * * * * libjwutil * * * * * * * * * * * * * */
+/* Copyright (C) 2023 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2022 J.W. Jagersma, see COPYING.txt for details */
 /* Copyright (C) 2021 J.W. Jagersma, see COPYING.txt for details */
 
@@ -8,14 +9,13 @@
 
 namespace jw::io
 {
-    struct io_error : public std::runtime_error { using runtime_error::runtime_error; };
-    struct overflow : public io_error { using io_error::io_error; };
-    struct parity_error : public io_error { using io_error::io_error; };
-    struct framing_error : public io_error { using io_error::io_error; };
-    struct line_break : public io_error { using io_error::io_error; };
-    struct timeout_error : public io_error { using io_error::io_error; };
+    struct io_error : std::runtime_error { using runtime_error::runtime_error; };
+    struct overflow : io_error { using io_error::io_error; };
+    struct parity_error : io_error { using io_error::io_error; };
+    struct framing_error : io_error { using io_error::io_error; };
+    struct timeout_error : io_error { using io_error::io_error; };
 
-    struct device_not_found : public std::runtime_error { using runtime_error::runtime_error; };
+    struct device_not_found : std::runtime_error { using runtime_error::runtime_error; };
 
     struct failure : std::ios::failure
     {
